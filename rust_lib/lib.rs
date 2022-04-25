21b21078839f48bd4f124df1b2ed4a517d5ed695
petr-tik <petr-tik@users.noreply.github.com>
2022-04-25 21:39:06 +0100

    Implement a rust API with inputs

--- rust_lib/lib.rs
+++ rust_lib/lib.rs
@@ -3,6 +3,13 @@ pub extern "C" fn my_favorite_number() -> i32 {
     4
 }
 
+#[no_mangle]
+pub extern "C" fn complex_maths(inp: i32) -> i32 {
+    let factor = 10i32;
+    // TODO aargh unsafe
+    return inp.checked_mul(factor).unwrap();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +17,10 @@ mod tests {
     #[test]
     fn test_my_favorite_number() {
         assert_eq!(4, my_favorite_number());
+    }
+
+    #[test]
+    fn test_my_complex_maths() {
+        assert_eq!(50, complex_maths(5));
     }
 }
