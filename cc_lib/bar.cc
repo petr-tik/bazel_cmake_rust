304af21abf729b5d2403693931c92554c0628ac2
petr-tik <petr-tik@users.noreply.github.com>
2022-04-25 21:39:24 +0100

    Link the static rust_lib into cc_lib
    it just works. Crazy.

--- cc_lib/bar.cc
+++ cc_lib/bar.cc
@@ -1,5 +1,13 @@
 #include "bar.h"
 
+extern "C" int complex_maths(int);
+
 int my_math() {
     return 10;
+}
+
+int combine_my_math_and_rust(int input) {
+    int input_by_ten = my_math() * input;
+
+    return complex_maths(input_by_ten);
 }
