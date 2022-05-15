d378bf77b57d429d4812982b154ddf1466487eda
petr-tik <petr-tik@users.noreply.github.com>
2022-05-15 13:42:59 +0100

    Make the python test print missing string
    Makes the test failure message more specific

--- cc_lib/test_rust_panic_overcomes_expect_any_throw.py
+++ cc_lib/test_rust_panic_overcomes_expect_any_throw.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 import sys
 import subprocess
+from typing import Set
+
+
+def check_strings_in_proc_output(proc_output, expected_strings: Set[str]) -> bool:
+    expected_strings_with_counts = {word: 0 for word in expected_strings}
+    for line in proc_output.splitlines():
+        for expected in expected_strings:
+            if expected in str(line):
+                expected_strings_with_counts[expected] += 1
+    assert all(
+        count > 0 for count in expected_strings_with_counts.values()
+    ), f"Didn't see: {list([expected for expected, count in expected_strings_with_counts.items() if count == 0])})"
 
 
 def test_panic_throw():
     cc_test_executable = sys.argv[1]
 
-    proc = subprocess.run([cc_test_executable],
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE,
-                          )
+    proc = subprocess.run(
+        [cc_test_executable],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     assert proc.returncode != 0
 
     expected_stderr_strings = set(
-        ["fatal runtime error: Rust panics must be rethrown'",
-         "core::option::Option<T>::unwrap"]
+        [
+            "fatal runtime error: Rust panics must be rethrown'",
+            "core::option::Option<T>::unwrap",
+        ]
     )
-
-    assert all(any(expected in str(line) for line in proc.stderr.splitlines())
-               for expected in expected_stderr_strings
-    ), "Missing one of the strings"
+    check_strings_in_proc_output(proc.stderr, expected_stderr_strings)
 
 
 if __name__ == "__main__":
